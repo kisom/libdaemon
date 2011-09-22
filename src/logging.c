092544a543960f212b2f236508b1fa59cc356c3f
Kyle Isom <coder@kyleisom.net>
2011-09-22 02:36:14 -0400

    src/logging.c: experiencing 80s flashback
    the people who run these computers suck. trying to get anything
    productive done is like trying to work on a 1400 baud modem - not
    quite a character every 3 seconds, more like a line every 3 seconds. it
    that in order to feel like i'm doing anything with my life, i'm resorting
    to the chunkhost web console because we can't have anything but office
    on these pieces of shit. i hate you guys so much.

---
+++ src/logging.c
@@ -0,0 +1,18 @@
+/****************************************************************************
+ * file: logging.c                                                          *
+ * author: kyle isom <coder@kyleisom.net>                                   *
+ * created: 2011-09-22                                                      *
+ * modified: 2011-09-22                                                     *
+ *                                                                          *
+ * logging functions for libdaemon                                          *
+ *                                                                          *
+ * it is released under an ISC / public domain dual-license; see any of the *
+ * header files or the file "LICENSE" (or COPYING) under the project root.  *
+ ****************************************************************************/
+#include <config.h>
+
+#ifdef _LINUX_SOURCE
+#include <sys.file.h>
+#endif
+#include <sys/type.h>
+
