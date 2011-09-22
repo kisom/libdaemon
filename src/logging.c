dcf652507e2f5acf901c2672d259c200042817ee
Kyle Isom <coder@kyleisom.net>
2011-09-22 22:15:12 +0300

    src/logging.c: write daemon_log and start vlog

--- src/logging.c
+++ src/logging.c
@@ -58,3 +58,46 @@ daemon_setlog(char *logfile)
         return retval;
 }
 
+int
+daemon_log(int priority, const char *message)
+{
+        int retval = EXIT_FAILURE;
+        ssize_t write_sz, buf_sz;
+        struct libdaemon_config *daemon_cfg;
+
+        buf_sz = (ssize_t)strlen(message);
+
+        daemon_cfg = daemon_getconfig();
+
+        if (NULL == daemon_cfg->logfile) {
+            syslog(priority, message, NULL);
+            retval = EXIT_SUCCESS;
+        } else {
+            write_sz = write(daemon_cfg->logfd, message, buf_sz);
+            if (write_sz == buf_sz)
+                retval = EXIT_SUCCESS;
+            write_sz = write(daemon_cfg->logfd, "\n", 1);
+        }
+        
+        return retval;
+}
+
+int
+daemon_vlog(int priority, const char *message, ...)
+{
+        va_list ap;
+        ssize_t write_sz, msg_sz;
+        struct libdaemon_config *daemon_cfg;
+
+        daemon_cfg = daemon_getconfig();
+        msg_sz = (ssize_t)strlen(message);
+
+        va_start(ap, msg);
+        
+        if (NULL == daemon_cfg->logfile)
+            vsyslog(priority, message, va_arg(ap, type));
+        else {
+            write_sz = write(daemon_cfg->logfd, message, ap);
+        }
+
+}
